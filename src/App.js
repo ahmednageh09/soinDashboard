b806995b83232447a979a5d14e7de6b1962cb73f
ahmednageh09 <110101445+ahmednageh09@users.noreply.github.com>
2024-03-15 07:11:44 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
-import { ToastContainer } from 'react-toastify'
 import { useSelector } from 'react-redux'
-import 'react-toastify/dist/ReactToastify.css'
+
 
 // Container
 const Layout = React.lazy(() => import('./layout/Layout'))
@@ -16,16 +15,14 @@ function App() {
   useEffect(() => {
     document.documentElement.dir = direction
   }, [direction])
-
-  const toastAutoCloseTime = 2000
+  
   return (
-    <Router>
-      <ToastContainer autoClose={toastAutoCloseTime} />
-      <Routes>
-        <Route exact path="/login" name="Login Page" element={<Login />} />
-        <Route path="*" name="Home" element={<Layout />} />
-      </Routes>
-    </Router>
+      <>
+        <Routes>
+          <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route path="*" name="Home" element={<Layout />} />
+        </Routes>
+      </>
   )
 }
 
